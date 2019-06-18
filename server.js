08a6ffeef69ce25ce06965f79e2863928fa70a61
Jaron Johnson <jjaronjohnson14@gmail.com>
2019-06-17 21:03:37 -0600

    Pushing up again after another heroku update

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 const mongoose = require("mongoose");
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -19,7 +16,12 @@ const routes = require("./controllers/comicControllers.js");
 
 app.use(routes);
 
-app.listen(PORT, function (){
-    console.log(`App now listing on localhost:${PORT}`);
-})
 
+// mongoose.connect("mongodb://localhost/scrapeThis", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrapeThis";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+app.listen(PORT, function () {
+  console.log("App running at localhost:" + PORT)
+})
